fd9417fee973a9c043338e81278cb814497fa12e
Alimullah <146616992+Alimullah1573@users.noreply.github.com>
2024-11-15 14:34:18 +0600

    Add files via upload

---
+++ GCD And LCM/0_GCD/GCD-Related-Problems/A. Complicated GCD.py
@@ -0,0 +1,22 @@
+
+#Codeforces
+#Problem link : https://codeforces.com/contest/664/problem/A
+
+def gcd(a, b):
+    while b:
+        a, b = b, a%b
+    return a
+
+
+def solve(a, b):
+    cur_gcd = a
+    for i in range(a+1, b+1):
+        cur_gcd = gcd(i, cur_gcd)
+        if cur_gcd == 1:
+            return 1
+    return cur_gcd
+
+
+if __name__ == "__main__":
+    a, b = map(int, input().split(" "))
+    print(solve(a,b))
